0761d12c8127d32ac2111b840f5d23544514a9fe
Maciej Szarlinski <mszarlinski@gmail.com>
2017-02-03 15:20:31 +0100

    Add two numbers

--- src/main/java/com/github/mszarlinski/tddkata/StringCalculator.java
+++ src/main/java/com/github/mszarlinski/tddkata/StringCalculator.java
@@ -1,11 +1,17 @@
 package com.github.mszarlinski.tddkata;
 
+import static java.util.Arrays.stream;
+
 /**
- * @author mszarlinski@bravurasolutions.com on 2017-02-03.
+ * @author mszarlinski on 2017-02-03.
  */
 public class StringCalculator {
 
     public int add(final String numbers) {
-        return Integer.parseInt(numbers);
+        final String[] ints = numbers.split(",");
+
+        return stream(ints)
+            .mapToInt(Integer::parseInt)
+            .sum();
     }
 }
--- src/test/groovy/com/github/mszarlinski/tddkata/StringCalculatorSpec.groovy
+++ src/test/groovy/com/github/mszarlinski/tddkata/StringCalculatorSpec.groovy
@@ -3,19 +3,27 @@ package com.github.mszarlinski.tddkata
 import spock.lang.Specification
 
 /**
- * @author mszarlinski@bravurasolutions.com on 2017-02-03.
+ * @author mszarlinski on 2017-02-03.
  */
 class StringCalculatorSpec extends Specification {
 
     StringCalculator calc = new StringCalculator()
 
-    def "Should take number and return it"() {
+    def "Should take number and return it as int"() {
         given:
             String numbers = "3"
         when:
             int sum = calc.add(numbers)
         then:
             sum == 3
+    }
 
+    def "Should take 2 numbers and return their sum"() {
+        given:
+            String numbers = "10,19"
+        when:
+            int sum = calc.add(numbers)
+        then:
+            sum == 29
     }
 }
